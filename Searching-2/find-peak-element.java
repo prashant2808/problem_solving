ff7f441c5f4e98b072f27553dcb0785f31faf097
prashant2808 <117907788+prashant2808@users.noreply.github.com>
2024-03-11 03:30:15 +0530

    Create find-peak-element.java

---
+++ Searching-2/find-peak-element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        if(n==1)return 0;
+        if(nums[0]>nums[1])return 0;
+        if(nums[n-1]>nums[n-2])return n-1;
+        int lo=1;
+        int hi=n-2;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1])
+            {
+                lo=mid+1;
+            }
+            else  //if(nums[mid]<nums[mid-1]&&nums[mid]>nums[mid+1]){
+                hi=mid-1;
+            
+          //  else if(nums[mid]<nums[mid-1]&&nums[mid]<nums[mid+1]){
+          //      hi=mid;
+          //  }
+        }
+        return lo;
+        
+    }
+}
