835fa1fdb685e41b68646bdc14111a0c5a915fb8
prashant2808 <117907788+prashant2808@users.noreply.github.com>
2024-04-10 21:17:59 +0530

    Add files via upload

---
+++ trees_6/lca_bst.java
@@ -0,0 +1,22 @@
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ class Solution {
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+      if(root==null)return null;
+      if((p.val>root.val&&q.val<root.val)||p.val<root.val&&q.val>root.val||root.val==p.val||root.val==q.val)
+      return root;
+      else if(p.val<root.val&&q.val<root.val)
+      return lowestCommonAncestor(root.left,p,q);
+       else return lowestCommonAncestor(root.right,p,q);
+
+      
+  }
+}
