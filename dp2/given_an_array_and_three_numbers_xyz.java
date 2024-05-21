1d28a1a9fe8b97ba0d394081f39648ab7beba1aa
Prashant Kumar <prashant578345@gmail.com>
2024-05-21 14:57:02 +0530

    Create given_an_array_and_three_numbers_xyz.java

---
+++ dp2/given_an_array_and_three_numbers_xyz.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class given_an_array_and_three_numbers_xyz {
+
+    static int find(int[] a, int x, int y, int z, int stage, int index,Map<String,Integer> memo) {
+        if (index >= a.length) {
+            return Integer.MIN_VALUE;
+        }
+          if (stage == 3) {
+            return 0; 
+        }
+        if (memo.containsKey(stage+"|"+index)) {
+            return memo.get(stage+"|"+index);
+        }
+        int currentval =stage == 0?x*a[index]:stage==1?y*a[index]:z*a[index];
+
+        int pick = currentval +find(a, x, y, z, stage + 1, index,memo);  
+        int nonpick =find(a, x, y, z, stage, index + 1,memo);
+
+        int ans = Math.max(pick,nonpick);
+        memo.put(stage+"|"+index,ans);
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr= new int [n];
+        for(int i=0;i<n;i++)arr[i]=sc.nextInt();
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        Map<String, Integer> memo = new HashMap<>();
+        System.out.println("Maximized value: " +find(arr, x, y, z,0,0,memo));
+    }
+}
